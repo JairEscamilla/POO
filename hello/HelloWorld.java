c92ed0322b4364ddf60ef38254e6343d7de1cdd0
Jair Escamilla <hernandez_yo14@outlook.es>
2019-08-21 17:16:55 -0500

    Agregando método

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld{
   public static void main(String[] args){
     Mensaje msg = new Mensaje();
     msg.saludos();
+    Mensaje msg2 = new Mensaje();
+    msg.nuevoMetodo();
   }
 }
--- hello/Mensaje.java
+++ hello/Mensaje.java
@@ -4,4 +4,7 @@ public class Mensaje{
   public void saludos(){
     System.out.println("Hola mundo");
   }
+  public void nuevoMetodo(){
+    System.out.println("Nuevo texto xd");
+  }
 }
