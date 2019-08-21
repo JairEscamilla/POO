0cc98acb03f5fa4a01de817d5b3531ec9ec51950
Jair Escamilla <hernandez_yo14@outlook.es>
2019-08-21 17:41:55 -0500

    JOptionPane

---
+++ Paneles/MensajeSaludo.java
@@ -0,0 +1,10 @@
+package Paneles;
+
+import javax.swing.JOptionPane;
+
+public class MensajeSaludo{
+  public static void main(String[] args){
+    //JOptionPane.showMessageDialog(null, "Hola mundo", JOptionPane.ERROR_MESSAGE);
+    JOptionPane.showMessageDialog(null, "Mensaje ", "Title", JOptionPane.INFORMATION_MESSAGE);
+  }
+}
