761963f63dafb85cc44bb925a88831c9a8d42169
Jair Escamilla <hernandez_yo14@outlook.es>
2019-10-06 19:36:57 -0500

    Evaluando la funcion

--- practica14/Eventos.java
+++ practica14/Eventos.java
@@ -3,6 +3,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JOptionPane;
 
+import practica13.PanelRadioButton;
+import practica6.Integral;
+import javax.swing.JOptionPane;
+
 public class Eventos implements ActionListener{
     public void actionPerformed(ActionEvent evento){
         try{
@@ -12,8 +16,21 @@ public class Eventos implements ActionListener{
             int a = Integer.parseInt(funcion.substring(0, funcion.indexOf("x2")));
             int b = Integer.parseInt(funcion.substring(funcion.indexOf("x2") + 2, funcion.indexOf("x1")));
             int c = Integer.parseInt(funcion.substring(funcion.indexOf("x1") + 2));
-            System.out.println(a + ", " + b + ", "+ c);//+ ", " + b + ", " +c);
-           // System.out.println(funcion.substring(funcion.indexOf("x2") + 2, funcion.indexOf("x1")));
+            System.out.println(a + ", " + b + ", "+ c);
+            if(evento.getSource() == practica13.PanelRadioButton.ninguna){
+                Integral area;
+                int li = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresar el limite inferior:"));
+                int ls = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresar el limite superior:"));
+                double dx = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresar el tamaño del salto dx: "));
+                area = new Integral(a, b, c, dx, li, ls);
+                double resultado = area.resolver();
+                JOptionPane.showMessageDialog(null, "El resultado de la integral es " + resultado, "Resultado integral", JOptionPane.INFORMATION_MESSAGE);
+            }
+            if(evento.getSource() == practica13.PanelRadioButton.media){
+                int value = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresar un valor donde evaluar la función: "));
+                int result = a*value*value + b*value + c;
+                JOptionPane.showMessageDialog(null, "El resultado de la evaluación es " + result, "Resultado evaluación", JOptionPane.INFORMATION_MESSAGE);
+            }
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, "Ha introducido mal los datos", "error", JOptionPane.ERROR_MESSAGE);
         }
--- practica6/Integral.java
+++ practica6/Integral.java
@@ -1,19 +1,22 @@
 package practica6;
 
 public class Integral{
-  int a, b;
+  int a, b, aa, bb, cc, li, ls;
   double dx;
-  public Integral(double dx){
-    this.a = -3;
-    this.b = 3;
+  public Integral(int aa, int bb, int cc, double dx, int li, int ls){
+    this.aa = aa;
+    this.bb = bb;
+    this.cc = cc;
     this.dx = dx;
+    this.li = li;
+    this.ls = ls;
   }
   private double f(double x){
-    return x;
+    return aa*x*x+bb*x+cc;
   }
   public double resolver(){
     double res = 0;
-    for(float i = a; i <= b; i+= dx){
+    for(float i = li; i <= ls; i+= dx){
       res+= dx*f(i);
     }
     return res;
