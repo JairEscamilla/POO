bdf52d00ef6ea4d738491a4b215118d3a80a73e0
Jair Escamilla <hernandez_yo14@outlook.es>
2019-10-06 19:15:14 -0500

    Solucionando algunos problemas

--- practica14/Eventos.java
+++ practica14/Eventos.java
@@ -9,13 +9,13 @@ public class Eventos implements ActionListener{
             boolean numbers = true;
             int ni, nf;
             String funcion = Informacion.txt.getText().toString();
-            int a = Integer.parseInt(funcion.substring(1, funcion.indexOf("x2")));
+            int a = Integer.parseInt(funcion.substring(0, funcion.indexOf("x2")));
             int b = Integer.parseInt(funcion.substring(funcion.indexOf("x2") + 2, funcion.indexOf("x1")));
             int c = Integer.parseInt(funcion.substring(funcion.indexOf("x1") + 2));
             System.out.println(a + ", " + b + ", "+ c);//+ ", " + b + ", " +c);
            // System.out.println(funcion.substring(funcion.indexOf("x2") + 2, funcion.indexOf("x1")));
         }catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Haa ocurrido un error", "error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Ha introducido mal los datos", "error", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
